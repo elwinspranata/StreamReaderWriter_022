3dde2ee9eec93dc044cc5e0abb17ead84bba5c5d
elwinspranata <elwinpranata02@gmail.com>
2023-06-21 15:26:09 +0700

    menunjukan file

--- ioFileWithPath_022/ioFileWithPath_022.cpp
+++ ioFileWithPath_022/ioFileWithPath_022.cpp
@@ -27,3 +27,15 @@ int main() {
 		// menulis dan memasukan nilai dari 'baris' ke dalam file 
 		outfile << baris << endl;
 	}
+	//selesai dalam menulis sekarang tutup filenya 
+	outfile.close();
+
+	//Membuka file dalam mode membaca 
+	ifstream infile;
+	//menunjukan ke sebuah file 
+	infile.open("contohfile.txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka 
+	if (infile.is_open())
+	{
